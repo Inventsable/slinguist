f226a00708ca467b70642bbdfd42d9b8788fd38e
inventsable <tom@inventsable.cc>
2024-08-08 19:51:56 -0700

    Recursive mapping of CK3's localization file structure in JSON for ease of cloning

--- src/parser/index.ts
+++ src/parser/index.ts
@@ -7,46 +7,66 @@ const path = require("path");
 import type { Decorator, TranslateNode } from "../../@types/index";
 import { format } from "date-fns";
 import { uuidv7 } from "uuidv7";
-import { readFile, writeFile } from "../utils/fs";
+import { isFolder, readDir, readFile, writeFile } from "../utils/fs";
 const yaml = require("js-yaml");
 
-async function test() {
-  let result = await writeYAMLFile();
-  console.log(result);
-  await writeFile("./sandbox/yaml.yml", result);
+const today = format(Date.now(), "yyyy.MM.dd");
+
+const ROOT_PATH =
+  "D:/SteamLibrary/steamapps/common/Crusader Kings III/game/localization/english";
+
+async function init() {
+  await generateLocalizationMap();
 }
 
-async function testParse() {
-  let result = await parseYAMLFile();
-  console.log(result);
-  await writeFile("./sandbox/yaml.json", JSON.stringify(result, null, 4));
+async function generateLocalizationMap() {
+  intro("Creating map of localization file structure");
+  s.start(`Mapping ${color.blue(color.underline(ROOT_PATH))}`);
+  let results = {
+    path: ROOT_PATH,
+    name: "english",
+    children: await readDirDeep(ROOT_PATH),
+  };
+  s.stop(`All done mapping ${color.blue(color.underline(ROOT_PATH))}`);
+  s.start(
+    `Writing results to ${color.blue(
+      color.underline(`./src/parser/map/${today}.json`)
+    )}`
+  );
+  await writeFile(
+    `./src/parser/map/${today}.json`,
+    JSON.stringify(results, null, 4)
+  );
+  s.stop(
+    `Results written to ${color.blue(
+      color.underline(`./src/parser/map/${today}.json`)
+    )}`
+  );
+  outro("All done");
 }
 
-async function parseYAMLFile() {
+const readDirDeep = async (targetPath: string): Promise<any> => {
+  const results = [];
   try {
-    const data = await readFile(
-      "./sandbox/tests/localization/hostages_l_english.yml"
-    );
-    console.log(data);
-    return yaml.load(data);
+    const children = await readDir(targetPath);
+    for (let child of children) {
+      if (isFolder(`${targetPath}/${child}`)) {
+        results.push({
+          name: child,
+          path: `${targetPath}/${child}`,
+          children: await readDirDeep(`${targetPath}/${child}`),
+        });
+      } else {
+        results.push({
+          name: child,
+          path: `${targetPath}/${child}`,
+        });
+      }
+    }
+    return results;
   } catch (err) {
     console.log(err);
   }
-}
+};
 
-async function writeYAMLFile() {
-  try {
-    // const targetFile = JSON.parse();
-    const targetFile = await readFile("./sandbox/yaml.json");
-    console.log(targetFile);
-    return yaml.dump(targetFile, {
-      indent: 1,
-      forceQuotes: true,
-      quotingType: '"',
-    });
-  } catch (err) {
-    console.log(err);
-  }
-}
-
-test();
+init();
